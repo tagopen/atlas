8b08f36e60e6b7517c9ffe209d6ae2ae35397502
marchik88 <marchik88@rambler.ru>
2017-04-04 18:01:53 +0300

    php bug

--- app/mail/mail.php
+++ app/mail/mail.php
@@ -1,102 +1,124 @@
 <?php
 
-  if (is_file('lib/class.phpmailer.php')) {
+if (is_file('lib/class.phpmailer.php')) {
     require_once("lib/class.phpmailer.php");
-  }
+}
 
-  if (is_file('lib/class.smtp.php')) {
+if (is_file('lib/class.smtp.php')) {
     require_once("lib/class.smtp.php");
-  }
+}
 
-  $http_host = $_SERVER['HTTP_HOST'];
+$http_host = $_SERVER['HTTP_HOST'];
 
-  if ( substr($http_host, 0, 4)=='www.') {
+if (substr($http_host, 0, 4) == 'www.') {
     $host_name = substr($http_host, 4);
-  } else {
+} else {
     $host_name = $http_host;
-  }
+}
 
-  define ('HTTP_SERVER', 'http://' . $http_host . '/');
-  define ('HOST_NAME', $host_name);
+define('HTTP_SERVER', 'http://' . $http_host . '/');
+define('HOST_NAME', $host_name);
 
-  $post = array( 
-    'host_name'     => HOST_NAME,
-    'host_dir'      => HTTP_SERVER,
-    );
+$post = array(
+    'host_name' => HOST_NAME,
+    'host_dir' => HTTP_SERVER
+);
 
-  $body = '';
-  
-  if (!empty($_POST["form"])) {
-    if(is_array($_POST["form"])) {
-      foreach ($_POST["form"] as $key => $value) {
-        $post['user_form'] = strip_tags($key);
-        $body .= 'Заявка с формы: ' . $post['user_form'] . chr(10) . chr(13);
-      }
+$body = '';
+
+if (!empty($_POST["form"])) {
+    if (is_array($_POST["form"])) {
+        foreach ($_POST["form"] as $key => $value) {
+            $post['user_form'] = strip_tags($key);
+            $body .= 'Заявка с формы: ' . $post['user_form'] . chr(10) . chr(13);
+        }
     }
-  }
+}
 
-  if (!empty($_POST["email"])) {
+if (!empty($_POST["email"])) {
     $post['user_email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $body .= 'Email адресс: ' . $post['user_email'] . chr(10) . chr(13);
-  }
+}
 
-  if (!empty($_POST["name"])) {
+if (!empty($_POST["name"])) {
     $post['user_name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
     $body .= 'Имя: ' . $post['user_name'] . chr(10) . chr(13);
-  }
+}
 
-  if (!empty($_POST["phone"])) {
-    $post['user_phone'] = filter_input(INPUT_POST,'phone', FILTER_SANITIZE_STRING);
+if (!empty($_POST["phone"])) {
+    $post['user_phone'] = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
     $body .= 'Телефон: ' . $post['user_phone'] . chr(10) . chr(13);
-  }
+}
 
-  if (!empty($_POST["message"])) {
+if (!empty($_POST["message"])) {
     $post['user_message'] = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
     $body .= 'Сообщение: ' . $post['user_message'] . chr(10) . chr(13);
-  }
+}
 
-  if (!empty($_POST["question"])) {
+if (!empty($_POST["question"])) {
     $post['user_speek'] = filter_input(INPUT_POST, 'question', FILTER_SANITIZE_STRING);
     $body .= 'Способ общения: ' . $post['user_speek'] . chr(10) . chr(13);
-  }
-  
+}
+
 /*  if (!empty($_POST["question"])) {
-    foreach ($_POST["question"] as $key => $values) {
-      if (is_array($values)) {
-        foreach ($values as $value) {
-          $post[$key] = strip_tags($value);
-          $body .= $post[$key] . chr(10) . chr(13);
-        }
-      } else {
-        $post[$key] = strip_tags($values);
-        $body .= $post[$key] . chr(10) . chr(13);
-      }
-    }
-  }*/
+foreach ($_POST["question"] as $key => $values) {
+if (is_array($values)) {
+foreach ($values as $value) {
+$post[$key] = strip_tags($value);
+$body .= $post[$key] . chr(10) . chr(13);
+}
+} else {
+$post[$key] = strip_tags($values);
+$body .= $post[$key] . chr(10) . chr(13);
+}
+}
+}*/
 
-  $mail = new PHPMailer();
 
-  $mail->CharSet      = 'UTF-8';
+$magnet = (($post['user_form'] == "Магнит - Хедер") || ($post['user_form'] == "Магнит - Узнайте 17 возможностей") || ($post['user_form'] == "Магнит - Узнайте 17 возможностей") || ($post['user_form'] == "Магнит - Выход с сайта"));
 
-  $mail->IsSendmail();
+if ($magnet) { 
+    if (!empty($_COOKIE["personalID"])) {
+    $post['user_id'] = filter_input(INPUT_POST, 'personalID', FILTER_SANITIZE_STRING);
+    $body .= 'Индентификатор пользователя: ' . $post['user_id'] . chr(10) . chr(13);
+} else {
+    $post['user_id'] = uniqid('_');
+    setcookie("personalID", $post['user_id'], time() + 60 * 5, "/");
+    $body .= 'Индентификатор пользователя: ' . $post['user_id'] . chr(10) . chr(13);
+}
+}
 
-  $from = 'no-reply@tagopen.com';
-  $to = "Artem2431@gmail.com";
-  $mail->SetFrom($from, HOST_NAME);
-  $mail->AddAddress($to);
+$mail = new PHPMailer();
 
-  $mail->isHTML(false);
+$mail->CharSet = 'UTF-8';
 
-  $mail->Subject      = "Новая заявка с сайта";
-  $mail->Body         = $body;
+$mail->IsSendmail();
 
-  if(!$mail->send()) {
+$from = 'no-reply@tagopen.com';
+$to   = "marchik88@rambler.ru";
+$mail->SetFrom($from, HOST_NAME);
+$mail->AddAddress($to);
+
+$mail->isHTML(false);
+
+$mail->Subject = "Заявка номер - " . $post['user_id'];
+$mail->Body    = $body;
+
+if (!$mail->send()) {
     echo 'Что-то пошло не так. ' . $mail->ErrorInfo;
     return false;
-  } else {
+} else {
     echo 'Сообщение отправлено';
-    header("Location: ../success.html");
-    return true;
-  }
+    if ($magnet) {
+        header("Location: ../oto.html");
+    } else {
+        if (isset($_COOKIE['personalID']) && ($post['user_form'] != "ОТО страница")) {
+            unset($_COOKIE['personalID']);
+            setcookie('personalID', '', time() - 3600, '/'); // empty value and old timestamp
+        }
+        header("Location: ../success.html");
+        return true;
+    }
+}
 
 ?>
